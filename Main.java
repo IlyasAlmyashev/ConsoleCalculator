f0565d166e8fa94f02d8e0f2903c60e7b6a647e4
IlyasAlmyashev <ilyas.almyashev.1991@gmail.com>
2021-10-16 12:35:31 +0300

    упрощение switch

--- Main.java
+++ Main.java
@@ -47,24 +47,13 @@ public class Main {
 
 class Calculator{
     public static int calc (int num1, int num2, String operator){
-        int result;
-        switch (operator) {
-            case "+":
-                result = num1 + num2;
-                break;
-            case "-":
-                result = num1 - num2;
-                break;
-            case "*":
-                result = num1 * num2;
-                break;
-            case "/":
-                result = num1 / num2;
-                break;
-            default:
-                throw new IllegalArgumentException("Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+,-,/,*) через пробелы.");
-        }
-        return result;
+        return switch (operator) {
+            case "+" -> num1 + num2;
+            case "-" -> num1 - num2;
+            case "*" -> num1 * num2;
+            case "/" -> num1 / num2;
+            default -> throw new IllegalArgumentException("Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+,-,/,*) через пробелы.");
+        };
     }
 }
 
@@ -85,29 +74,18 @@ class ConvertManager {
         return res.toString();
     }
     public static int romanToArabic(String roman) {
-        switch (roman) {
-            case "I":
-                return 1;
-            case "II":
-                return 2;
-            case "III":
-                return 3;
-            case "IV":
-                return 4;
-            case "V":
-                return 5;
-            case "VI":
-                return 6;
-            case "VII":
-                return 7;
-            case "VIII":
-                return 8;
-            case "IX":
-                return 9;
-            case "X":
-                return 10;
-            default:
-                return -1;
-        }
+        return switch (roman) {
+            case "I" -> 1;
+            case "II" -> 2;
+            case "III" -> 3;
+            case "IV" -> 4;
+            case "V" -> 5;
+            case "VI" -> 6;
+            case "VII" -> 7;
+            case "VIII" -> 8;
+            case "IX" -> 9;
+            case "X" -> 10;
+            default -> -1;
+        };
     }
 }
